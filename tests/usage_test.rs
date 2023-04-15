33ad7c6d356347919913069f28fea974253a8437
ugandalf <driwer99@interia.pl>
2023-04-15 02:20:27 +0200

    Single file kind of works

--- tests/usage_test.rs
+++ tests/usage_test.rs
@@ -1,13 +1,16 @@
 use capnp_import::capnp_import;
 
+mod example_capnp {
+    capnp_import::capnp_import!(["tests/example.capnp"]);
+    capnp_import::capnp_import!(["tests/folder-test"]);
+}
+
 #[test]
 fn basic_file_test() {
-    capnp_import!(["tests/example.capnp"]);
-    //use person;
+    use example_capnp::person;
 }
 
 #[test]
 fn folder_test() {
-    //capnp_import!(["tests/folder-test"]);
     //use person;
 }
